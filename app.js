a1dc234b9ef42156c6a8de4c127a8747bd051d2d
Juanzgc <juanzgc@bu.edu>
2019-12-08 20:21:28 -0500

    h

--- app.js
+++ app.js
@@ -31,17 +31,17 @@ var level = require('level')
 var db = level('laps')
 
 // 2) Put a key & value
-db.put('name', 'Level3', function (err) {
-  if (err) return console.log('Ooops!', err) // some kind of I/O error
+// db.put('name', 'Level3', function (err) {
+//   if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-  // 3) Fetch by key
-  db.get('name', function (err, value) {
-    if (err) return console.log('Ooops!', err) // likely the key was not found
+//   // 3) Fetch by key
+//   db.get('name', function (err, value) {
+//     if (err) return console.log('Ooops!', err) // likely the key was not found
 
-    // Ta da!
-    console.log('name=' + value)
-  })
-})
+//     // Ta da!
+//     console.log('name=' + value)
+//   })
+// })
 
 db.createReadStream()
 .on('data', function (data) {
