846ceecc5c72733903cbdbc3aa74ebc1e33a00a7
Juanzgc <juanzgc@bu.edu>
2019-12-08 20:27:16 -0500

    Lets go

--- app.js
+++ app.js
@@ -31,17 +31,21 @@ var level = require('level')
 var db = level('laps')
 
 // 2) Put a key & value
-// db.put('name', 'Level3', function (err) {
-//   if (err) return console.log('Ooops!', err) // some kind of I/O error
+var obj = {
+  "time": 1,
+  "beacon": "b1"
+}
+db.put('name', obj, function (err) {
+  if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-//   // 3) Fetch by key
-//   db.get('name', function (err, value) {
-//     if (err) return console.log('Ooops!', err) // likely the key was not found
+  // 3) Fetch by key
+  db.get('name', function (err, value) {
+    if (err) return console.log('Ooops!', err) // likely the key was not found
 
-//     // Ta da!
-//     console.log('name=' + value)
-//   })
-// })
+    // Ta da!
+    console.log('name=' + value)
+  })
+})
 
 db.createReadStream()
 .on('data', function (data) {
